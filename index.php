91b967aab6842f4e38d510031db46e8706a1529b
pescanovaisnotbald <joanotruiz@gmail.com>
2025-10-22 18:18:36 +0200

    test2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     </head>
     <body>
         <?php
-        //echo "test"
+        //echo "test2";
 		include_once __DIR__ . "/connectaBD.php";  
 		$con = connectaBD();
 		$query_graus = "SELECT id,nom FROM graus";
